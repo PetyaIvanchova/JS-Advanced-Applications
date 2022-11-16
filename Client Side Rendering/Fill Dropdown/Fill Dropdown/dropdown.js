26fed8bc8a5bd7a2fa45b02318139de8e929ef93
Petya Ivanchova <105751255+PetyaIvanchova@users.noreply.github.com>
2022-11-16 19:23:06 +0200

    Create dropdown.js

---
+++ Client Side Rendering/Fill Dropdown/Fill Dropdown/dropdown.js
@@ -0,0 +1,38 @@
+import {html, render} from '../node_modules/lit-html/lit-html.js'
+const url = "http://localhost:3030/jsonstore/advanced/dropdown";
+const form = document.querySelector("form");
+const root = document.getElementById("menu");
+form.addEventListener("submit", onSubmit)
+
+onLoadContent();
+
+async function onLoadContent(){
+    const response = await fetch(url);
+    const data = await response.json();
+
+    const result = Object.values(data).map(x=>createOptionTemplate(x));
+    render(result, root);
+}
+
+function createOptionTemplate(option){
+    return html`<option value=${option._id}> ${option.text}</option>`;
+}
+
+function onSubmit(e){
+    e.preventDefault();
+    const value = document.getElementById("itemText").value;
+    value && addItem(value);
+}
+
+async function addItem(data){
+    const response = await fetch(url, {
+        method: "POST",
+        headers: {
+            'Content-Type': "Application/json"
+        },
+        body: JSON.stringify({text: data})
+    })
+
+    form.reset();
+    onLoadContent();
+}
